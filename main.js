183ebf249c95a2acdf5858db42468d6ecb366461
Stormtrooper66 <redstoner66@gmail.com>
2023-02-06 22:25:45 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
-const {Client, GatewayIntentBits, Collection, DiscordAPIError, time } = require('discord.js')
+const {Client, GatewayIntentBits, Collection, DiscordAPIError } = require('discord.js')
 const Discord = require('discord.js');
 
 const dotenv = require('dotenv')
 dotenv.config()
-const token = `${process.env['TOKEN']}`
+const token = process.env.TOKEN
 const version = process.env.VERSION
 const prefix = process.env.PREFIX
 
@@ -26,7 +26,7 @@ const client = new Client({
     ]
   });
 
-//Gets all the bot commands and prepares them
+
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
@@ -35,20 +35,17 @@ for(const file of commandFiles){
   client.commands.set(runcommand.name, runcommand);
 }
 
-//Loads any saved game data
 const savedData = require('./Saved_Data.json');
 
-//message once bot is booted up
 client.on('ready', () => {
     console.log(`Logged in as: ${client.user.tag}\nBot Online!`)
 })
 
-//actual command tracking
 client.on('messageCreate', (message) => {
-  if(message.author.bot) return; //bot won't do anything from it's own commands
+  if(message.author.bot) return;
 
   //objective checking
-  if(message.channel.id == 958755910744956948 || message.channel.id == 900130926846148608){ //channel ID number for the channel players are posting objectives into
+  if(message.channel.id == 958755910744956948 || message.channel.id == 900130926846148608){
     const args = message.content.slice(prefix.length).split(/ +/);
 
     client.commands.get("countObj").execute(message,args,savedData);
@@ -66,5 +63,9 @@ client.on('messageCreate', (message) => {
   }
   
 })
+
+
+
+
 
 client.login(token)
