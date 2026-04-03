634c24819cf815922aa30324916d4b02207f1b7a
Stormtrooper66 <redstoner66@gmail.com>
2026-04-03 11:35:52 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const Discord = require('discord.js');
 const dotenv = require('dotenv')
 dotenv.config()
 const token = process.env.TOKEN
-const version = process.env.VERSION
 const prefix = process.env.PREFIX
+
+const version = "2.5.5";
 
 const client = new Client({
     partials: [
@@ -26,7 +27,7 @@ const client = new Client({
     ]
   });
 
-
+//command execution files
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
@@ -35,21 +36,46 @@ for(const file of commandFiles){
   client.commands.set(runcommand.name, runcommand);
 }
 
+// slash command execution files
+client.slash_commands = new Discord.Collection();
+const slashcommandFiles = fs.readdirSync('./slash_commands/').filter(file => file.endsWith('.js'));
+for(const file of slashcommandFiles){
+  const runslashcommand = require(`./slash_commands/${file}`);
+
+  client.slash_commands.set(runslashcommand.name, runslashcommand);
+}
+
 const savedData = require('./Saved_Data.json');
+savedData.botVersion = version;
 
 client.on('ready', () => {
-    console.log(`Logged in as: ${client.user.tag}\nBot Online!`)
+    console.log(`Logged in as: ${client.user.tag}\nBot Online!\n\nVersion ${version}`);
+    
+    client.user.setPresence({
+      activities: [{ name: 'HvZ!' }],
+      status: 'online',
+    });  
 })
 
+//Chat based commands aka using !
 client.on('messageCreate', (message) => {
   if(message.author.bot) return;
 
   //objective checking
-  if(message.channel.id == 958755910744956948 || message.channel.id == 900130926846148608){
+  if(message.channel.name == "objectives" || message.channel.name == "Objectives" || message.channel.name == "objective" || message.channel.name == "Objective"){
     const args = message.content.slice(prefix.length).split(/ +/);
-
-    client.commands.get("countObj").execute(message,args,savedData);
-
+    try{
+      client.commands.get("countObj").execute(message,args,savedData);
+    }catch(error){
+      return message.reply(
+        {embeds: [{
+          description: `Something broke. try again or notify an admin if this issue persists.`,
+          color: 	16776960 //yellow
+          }], 
+          ephemeral:true  
+        }
+      );
+    }
     return;
   }
   //command checking
@@ -58,14 +84,57 @@ client.on('messageCreate', (message) => {
   const args = message.content.slice(prefix.length).split(/ +/);
   const runcommand = args.shift().toLowerCase();
 
-  if(client.commands.get(runcommand)){
-    client.commands.get(runcommand).execute(message,args,savedData);
+  try{
+    if(client.commands.get(runcommand)){
+      client.commands.get(runcommand).execute(message,args,savedData);
+    }
+  }catch (error){
+    console.log(`Error on Command input.\n\n${error.toString()}\n`);
+    return message.reply(
+      {embeds: [{
+        description: `There was an error. Try again or contact an admin if this issue persists.`,
+        color: 	16776960 //yellow
+        }], 
+        ephemeral:true  
+      }
+    );
   }
   
 })
 
 
+//Slash based commands, aka auto-completable ones
+client.on('interactionCreate', (interaction) => {
 
+  if (!interaction.isChatInputCommand()) return;
 
+  const args = 0//message.content.slice(prefix.length).split(/ +/);
+  const slashcommand = interaction.commandName.toString();
+
+  try{
+    if(client.slash_commands.get(slashcommand)){
+      client.slash_commands.get(slashcommand).execute(interaction,args,savedData);
+    }else{
+      return interaction.reply(
+        {embeds: [{
+          description: `\"${slashcommand}\" not found. Contact an admin if this issue persists.`,
+          color: 	16776960 //yellow
+          }], 
+          ephemeral:true  
+        }
+      );
+    }
+  }catch (error){
+    return interaction.reply(
+      {embeds: [{
+        description: `\"${slashcommand}\" not run. Contact an admin if this issue persists.`,
+        color: 	16776960 //yellow
+        }], 
+        ephemeral:true  
+      }
+    );
+  }
+  
+})
 
 client.login(token)
